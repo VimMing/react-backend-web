95044b601d9fb0d6a776b8b8bf1c0887e7d4fb39
VimMing <vimmingshe@gmail.com>
2022-05-29 11:17:20 +0800

    chore: 添加配置文件

--- src/features/user/userAPI.ts
+++ src/features/user/userAPI.ts
@@ -1,10 +1,16 @@
 import axios from "axios";
+
+const API = axios.create({
+  baseURL: process.env.REACT_APP_API,
+  timeout: 5000,
+});
+
 export function loginAPI(payload: {
   username: string;
   password: string;
 }): Promise<any> {
-  return axios.post(
-    "https://birthday.codehub.store/api/user/use-password/login",
+  return API.post(
+    "/user/use-password/login",
     payload
   );
 }
