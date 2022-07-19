3d6e7167a5916bf1e1c8826fa4b783f96d9fadb9
VimMing <vimmingshe@gmail.com>
2022-07-19 21:50:04 +0800

    feat: add token.ts file

---
+++ src/utils/token.ts
@@ -0,0 +1,24 @@
+const TOKEN_KEY = "TOKEN_KEY";
+
+export function getToken() {
+  const data = localStorage.getItem(TOKEN_KEY);
+  return data ? JSON.parse(data) : {};
+}
+
+export function appendToken(config: {headers: object}){
+  let res = {...config}
+  Object.assign(res.headers, getToken());
+  return res;
+}
+
+export function setToken(token:string) {
+  const data = {
+    Authorization: `${token}`
+  };
+  localStorage.setItem(TOKEN_KEY, JSON.stringify(data));
+}
+
+export function removeToken() {
+  localStorage.removeItem(TOKEN_KEY);
+}
+
