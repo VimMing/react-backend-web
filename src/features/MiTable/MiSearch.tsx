134faab89cb5b2759b32c805fdcb70fae6e2ef21
VimMing <vimmingshe@gmail.com>
2022-11-26 20:02:43 +0800

    feat: refine search form

--- src/features/MiTable/MiSearch.tsx
+++ src/features/MiTable/MiSearch.tsx
@@ -23,8 +23,8 @@ const logics = [
     label: '或者',
   },
 ]
-function LogicSelect() {
-  return (<BasicSelect label='逻辑' options={logics}></BasicSelect>)
+function LogicSelect(props: Omit<BasicSelectProps, 'label' | 'options'>) {
+  return <BasicSelect label="逻辑" options={logics} {...props}></BasicSelect>
 }
 const opts = [
   {
@@ -56,35 +56,33 @@ const opts = [
     label: '小于等于',
   },
 ]
-function OperatorSelect() {
-  return (<BasicSelect label='操作符' options={opts}></BasicSelect>)
+function OperatorSelect(props: Omit<BasicSelectProps, 'label' | 'options'>) {
+  return <BasicSelect label="操作符" options={opts} {...props}></BasicSelect>
 }
 
 type BasicSelectProps = {
-  label: string,
-  value?: string,
-  options: Array<{value: string | number, label: string | number}>
+  label: string
+  value?: string
+  onChange?: (val: string | number) => void
+  options: Array<{ value: string | number; label: string | number }>
 }
 
-function BasicSelect(props:BasicSelectProps){
+function BasicSelect(props: BasicSelectProps) {
   const [value, setValue] = useState<string>('')
   useEffect(() => {
-    if(props.value != null){
+    if (props.value != null) {
       setValue(props.value)
     }
   }, [props.value])
 
   const handleChange = (event: SelectChangeEvent) => {
     setValue(event.target.value as string)
+    props.onChange && props.onChange(event.target.value as string)
   }
   return (
     <FormControl fullWidth size="small">
       <InputLabel>{props.label}</InputLabel>
-      <Select
-        value={value}
-        label={props.label}
-        onChange={handleChange}
-      >
+      <Select value={value} label={props.label} onChange={handleChange}>
         {props.options.map((item) => (
           <MenuItem value={item.value}>{item.label}</MenuItem>
         ))}
@@ -93,42 +91,90 @@ function BasicSelect(props:BasicSelectProps){
   )
 }
 
-
 type MiSearchProps<T> = {
   columns: Columns<T>
 }
 export default function MiSearch<T>(props: MiSearchProps<T>) {
-  let x: { b?: string; a?: number } = { b: 'hello world', a: 1 }
-  const [templateSearchForm, setTemplateSearchForm] = useState(x)
+  const [templateSearchForm, setTemplateSearchForm] = useState<
+    Array<{
+      fieldValue: string | number
+      fieldCode: string
+      operator: string
+      logic: string
+    }>
+  >([])
+  const columnOptions = props.columns.map((i) => ({
+    label: i.label,
+    value: i.key,
+  }))
   useEffect(() => {
     console.log('Mi Search', props.columns)
     console.log(templateSearchForm)
+    setTemplateSearchForm([
+      {
+        fieldValue: '',
+        fieldCode: columnOptions[0].value as string,
+        operator: opts[0].value,
+        logic: logics[0].value,
+      },
+    ])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [props.columns.length])
+  useEffect(() => {
+    console.log('组件刷新', templateSearchForm);
   })
-  const columnOptions = props.columns.map(i => ({label: i.label, value: i.key}))
+  const handleChange = (
+    index: number,
+    field: 'fieldValue' | 'fieldCode' | 'operator' | 'logic',
+    value: number | string
+  ): void => {
+    // console.log(templateSearchForm)
+    templateSearchForm[index][field] = value as any
+    console.log(templateSearchForm)
+    setTemplateSearchForm([...templateSearchForm])
+  }
   return (
     <Box pb={2} pt={2}>
       <Grid container spacing={2}>
         <Grid item xs={12} md={10}>
-          <Stack spacing={1} direction="row">
-            <BasicSelect label="搜索列" options={columnOptions}></BasicSelect>
-            <OperatorSelect></OperatorSelect>
-            <TextField
-              size="small"
-              fullWidth
-              label="搜索值"
-              variant="outlined"
-            />
-            <LogicSelect></LogicSelect>
-          </Stack>
+          {templateSearchForm.map((template, templateIndex) => {
+            return (
+              <Stack spacing={1} direction="row">
+                <BasicSelect
+                  label="搜索列"
+                  options={columnOptions}
+                  value={template.fieldCode}
+                  onChange={(val) =>
+                    handleChange(templateIndex, 'fieldCode', val)
+                  }
+                ></BasicSelect>
+                <OperatorSelect
+                  value={template.operator}
+                  onChange={(val) =>
+                    handleChange(templateIndex, 'operator', val)
+                  }
+                ></OperatorSelect>
+                <TextField
+                  size="small"
+                  fullWidth
+                  label="搜索值"
+                  value={template.fieldValue}
+                  onChange={event => handleChange(templateIndex, 'fieldValue', event.target.value as string)}
+                  variant="outlined"
+                />
+                <LogicSelect
+                  value={template.logic}
+                  onChange={(val) =>
+                    handleChange(templateIndex, 'logic', val)
+                  }
+                ></LogicSelect>
+              </Stack>
+            )
+          })}
         </Grid>
         <Grid item xs={12} md={2}>
           <Stack spacing={2} direction="row">
-            <Button
-              variant="contained"
-              onClick={() => setTemplateSearchForm({ a: Math.random() })}
-            >
-              查询
-            </Button>
+            <Button variant="contained">查询</Button>
             <Button variant="outlined">重置</Button>
           </Stack>
         </Grid>
--- src/features/layout/sidebar.tsx
+++ src/features/layout/sidebar.tsx
@@ -5,9 +5,6 @@ import ListItemText from "@mui/material/ListItemText";
 import ListSubheader from "@mui/material/ListSubheader";
 import DashboardIcon from "@mui/icons-material/Dashboard";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import PeopleIcon from "@mui/icons-material/People";
-import BarChartIcon from "@mui/icons-material/BarChart";
-import LayersIcon from "@mui/icons-material/Layers";
 import AssignmentIcon from "@mui/icons-material/Assignment";
 import { useNavigate, useMatch } from "react-router-dom";
 
@@ -23,29 +20,11 @@ export const MainListItems = () => {
         </ListItemIcon>
         <ListItemText primary="Dashboard" />
       </ListItemButton>
-      <ListItemButton onClick={() => navigate("/user-list")}>
+      <ListItemButton selected={!!useMatch("/user-list")} onClick={() => navigate("/user-list")}>
         <ListItemIcon>
           <ShoppingCartIcon />
         </ListItemIcon>
-        <ListItemText primary="Orders" />
-      </ListItemButton>
-      <ListItemButton>
-        <ListItemIcon>
-          <PeopleIcon />
-        </ListItemIcon>
-        <ListItemText primary="Customers" />
-      </ListItemButton>
-      <ListItemButton>
-        <ListItemIcon>
-          <BarChartIcon />
-        </ListItemIcon>
-        <ListItemText primary="Reports" />
-      </ListItemButton>
-      <ListItemButton>
-        <ListItemIcon>
-          <LayersIcon />
-        </ListItemIcon>
-        <ListItemText primary="Integrations" />
+        <ListItemText primary="微信用户" />
       </ListItemButton>
     </React.Fragment>
   );
@@ -61,18 +40,6 @@ export const secondaryListItems = (
         <AssignmentIcon />
       </ListItemIcon>
       <ListItemText primary="Current month" />
-    </ListItemButton>
-    <ListItemButton>
-      <ListItemIcon>
-        <AssignmentIcon />
-      </ListItemIcon>
-      <ListItemText primary="Last quarter" />
-    </ListItemButton>
-    <ListItemButton>
-      <ListItemIcon>
-        <AssignmentIcon />
-      </ListItemIcon>
-      <ListItemText primary="Year-end sale" />
     </ListItemButton>
   </React.Fragment>
 );
