3909a70637be63bc9798848d71bfae5ff328563a
VimMing <vimmingshe@gmail.com>
2022-11-26 11:06:57 +0800

    feat: refine search form

--- src/features/MiTable/MiSearch.tsx
+++ src/features/MiTable/MiSearch.tsx
@@ -5,6 +5,7 @@ import {
   Button,
   FormControl,
   MenuItem,
+  TextField,
   Select,
   InputLabel,
   SelectChangeEvent,
@@ -23,28 +24,7 @@ const logics = [
   },
 ]
 function LogicSelect() {
-  const [age, setAge] = useState('')
-
-  const handleChange = (event: SelectChangeEvent) => {
-    console.log(event)
-    setAge(event.target.value as string)
-  }
-  return (
-    <FormControl fullWidth size="small">
-      <InputLabel id="logic-select-label">Age</InputLabel>
-      <Select
-        labelId="logic-select-label"
-        id="logic-select"
-        value={age}
-        label="Age"
-        onChange={handleChange}
-      >
-        <MenuItem value={10}>Ten</MenuItem>
-        <MenuItem value={20}>Twenty</MenuItem>
-        <MenuItem value={30}>Thirty</MenuItem>
-      </Select>
-    </FormControl>
-  )
+  return (<BasicSelect label='逻辑' options={logics}></BasicSelect>)
 }
 const opts = [
   {
@@ -76,6 +56,44 @@ const opts = [
     label: '小于等于',
   },
 ]
+function OperatorSelect() {
+  return (<BasicSelect label='操作符' options={opts}></BasicSelect>)
+}
+
+type BasicSelectProps = {
+  label: string,
+  value?: string,
+  options: Array<{value: string | number, label: string | number}>
+}
+
+function BasicSelect(props:BasicSelectProps){
+  const [value, setValue] = useState<string>('')
+  useEffect(() => {
+    if(props.value != null){
+      setValue(props.value)
+    }
+  }, [props.value])
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setValue(event.target.value as string)
+  }
+  return (
+    <FormControl fullWidth size="small">
+      <InputLabel>{props.label}</InputLabel>
+      <Select
+        value={value}
+        label={props.label}
+        onChange={handleChange}
+      >
+        {props.options.map((item) => (
+          <MenuItem value={item.value}>{item.label}</MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  )
+}
+
+
 type MiSearchProps<T> = {
   columns: Columns<T>
 }
@@ -83,19 +101,27 @@ export default function MiSearch<T>(props: MiSearchProps<T>) {
   let x: { b?: string; a?: number } = { b: 'hello world', a: 1 }
   const [templateSearchForm, setTemplateSearchForm] = useState(x)
   useEffect(() => {
-    console.log('Mi Search')
+    console.log('Mi Search', props.columns)
     console.log(templateSearchForm)
   })
+  const columnOptions = props.columns.map(i => ({label: i.label, value: i.key}))
   return (
     <Box pb={2} pt={2}>
-      <Grid container>
-        <Grid xs={12} md={10}>
-        <Stack spacing={1} direction="row">
-          <LogicSelect></LogicSelect> 
-          <LogicSelect></LogicSelect> 
-        </Stack>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={10}>
+          <Stack spacing={1} direction="row">
+            <BasicSelect label="搜索列" options={columnOptions}></BasicSelect>
+            <OperatorSelect></OperatorSelect>
+            <TextField
+              size="small"
+              fullWidth
+              label="搜索值"
+              variant="outlined"
+            />
+            <LogicSelect></LogicSelect>
+          </Stack>
         </Grid>
-        <Grid xs={12} md={2}>
+        <Grid item xs={12} md={2}>
           <Stack spacing={2} direction="row">
             <Button
               variant="contained"
